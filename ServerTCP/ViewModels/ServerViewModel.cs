0a8aa0989ca6cd71d34bc97a92c48a06d423a12d
MichaelTainan <michaeltainan@gmail.com>
2023-03-20 22:15:51 +0800

    Refactoring:ServerViewModel, use linq replace foreach()

--- ServerTCP/ViewModels/ServerViewModel.cs
+++ ServerTCP/ViewModels/ServerViewModel.cs
@@ -11,8 +11,8 @@ namespace ServerTCP.ViewModels
 {
     public class ServerViewModel : INotifyPropertyChanged
     {
-        private IListenManager listenManager;
-        private IClientManager clientManager;
+        private readonly IListenManager listenManager;
+        private readonly IClientManager clientManager;
         private ObservableCollection<ClientViewModel> clients;
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -102,14 +102,10 @@ namespace ServerTCP.ViewModels
 
             Application.Current.Dispatcher.Invoke(() =>
             {
-                foreach (var client in clients)
+                if (!clients.Any(x => x.IP == clientInfo.IP && x.Port == clientInfo.Port))
                 {
-                    if (client.IP == clientInfo.IP && client.Port == clientInfo.Port)
-                    {
-                        return;
-                    }
+                    clients.Add(new ClientViewModel(clientInfo));
                 }
-                clients.Add(new ClientViewModel(clientInfo));
             });
         }
 
@@ -117,13 +113,10 @@ namespace ServerTCP.ViewModels
         {
             Application.Current.Dispatcher.Invoke(() =>
             {
-                foreach (var client in clients)
+                var client = clients.FirstOrDefault(x => x.IP == clientInfo.IP && x.Port == clientInfo.Port);
+                if (client != null) 
                 {
-                    if (client.IP == clientInfo.IP && client.Port == clientInfo.Port)
-                    {
-                        client.FileName = clientInfo.FileName;
-                        break;
-                    }
+                    client.FileName = clientInfo.FileName;
                 }
             });
         }
@@ -132,13 +125,10 @@ namespace ServerTCP.ViewModels
         {
             Application.Current.Dispatcher.Invoke(() =>
             {
-                foreach (var client in clients)
+                var client = clients.FirstOrDefault(x => x.IP == clientInfo.IP && x.Port == clientInfo.Port);
+                if (client != null) 
                 {
-                    if (client.IP == clientInfo.IP && client.Port == clientInfo.Port)
-                    {
-                        clients.Remove(client);
-                        break;
-                    }
+                    clients.Remove(client);
                 }
             });
         }
@@ -146,34 +136,6 @@ namespace ServerTCP.ViewModels
         private void RaisePropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-
-    internal class RelayCommand : ICommand
-    {
-        private readonly Action execute;
-        private readonly Func<bool> canExecute;
-
-        public event EventHandler CanExecuteChanged
-        {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
-        }
-
-        public RelayCommand(Action execute, Func<bool> canExecute = null)
-        {
-            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
-            this.canExecute = canExecute;
-        }
-
-        public bool CanExecute(object parameter)
-        {
-            return canExecute == null || canExecute();
-        }
-
-        public void Execute(object parameter)
-        {
-            execute();
         }
     }
 }
